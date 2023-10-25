d696a88e31bc617fd98f21a51f70baefffb5e825
Kaushik Kudchadkar <118542939+Kaushik2201@users.noreply.github.com>
2023-10-26 00:05:56 +0530

    Updated main.py - Small Change in spelling

--- Bot/main.py
+++ Bot/main.py
@@ -37,165 +37,136 @@ async def check_database_for_updates():
         except Exception as e:
             print(f"Error checking the database: {e}")
 
-
-
-# Words of Commnads asked
+# Words of Commands asked
 sig_words = ["sig", "groups", "interestgroups","wecsigs" , "special interest groups", "interest groups", "group"]
 members_words =["members", "people", "ppl", "team" ,"seniors"] 
-upevents_words =["upcomming events", "new events" ,"comming events", "next events", "curr" ,"current"] 
-pevents_words = ["old events", "previous events", "past events", "prev events", "done events", "prev"]
-con = ["who is the convenor of the web club", "whoistheconvenorofthewebclub", "con" ,"convenor"]
-comeve = ["what is the upcomming event","comming up next"]
-thanks_words = ["thank you", "thanks", "appreciate ", "gratitude","thankyou"]
-
-
+upevents_words =["upcommingevents", "newevents" ,"commingevents", "nextevents", "curr" ,"current"] 
+pevents_words = ["oldevents", "previousevents", "pastevents", "prevevents", "doneevents", "prev"]
+con = ["whoistheconvenorofthewebclub"]
+comeve = ["whatistheupcommingevent","commingupnext"]
+thanks_words = ["thankyou", "thanks", "appreciate", "gratitude"]
 
 @client.event
 async def on_ready():
   print(f"Logged in as {client.user}")
-
-
-
 
 # This is the main function used here for all the commands
 @client.event
 async def on_message(message):  
   if message.author == client.user:
     return
-  
-  # To avoid case sensitivity  
-  msg = message.content.lower()
-  msg = message.content.replace(" ", "")
 
+  msg = message.content.lower().replace(" ", "")
 
   if msg.startswith("/hello"):
-    await msg.channel.send("Hello ! welcome to this server and I am Info Bot and \n I am here to help you out with your queries releted to WEC Nitk")
-
+    await message.channel.send("Hello! Welcome to this server. I am Info Bot, and I am here to help you with your queries related to WEC NITK.")
 
   if msg.startswith(".wec"):
     if any(word in msg for word in sig_words):
       # Retrieve values from the database
       conn = mysql.connector.connect(**db_config)
       cursor = conn.cursor()
-
       cursor.execute("SELECT * FROM sig")
       values = cursor.fetchall()
-
       cursor.close()
       conn.close()
-      values_str = ' \n\n'.join(str(value[0]) for value in values)
-      await message.channel.send(f"Sigs! sure here you go \n\n WEC NITK has 4 SIGS \n\n {values_str}")
-
+      values_str = '\n\n'.join(str(value[0]) for value in values)
+      await message.channel.send(f"Sigs! Sure, here you go:\n\nWEC NITK has 4 SIGs:\n{values_str}")
 
   if msg.startswith(".wec"):
     if any(word in msg for word in members_words):
-
       conn = mysql.connector.connect(**db_config)
       cursor = conn.cursor()
       cursor.execute("SELECT Name FROM members where Position = 'Core'")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
-      await message.channel.send(f"The Core of WEC NitK for year 2023-2024 \n\n {values_str}")
+      await message.channel.send(f"The Core of WEC NitK for year 2023-2024:\n{values_str}")
 
       cursor.execute("SELECT Name FROM membersaldo where Grp = 'Algorithms'")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
-      await message.channel.send(f"Members of Algorithm sig \n\n {values_str}")
+      await message.channel.send(f"Members of Algorithm sig:\n{values_str}")
 
       cursor.execute("SELECT Name FROM membersgdsc where grp = 'GDSC'")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
-      await message.channel.send(f"Members of Google Development Students Club sig  \n\n {values_str}")
+      await message.channel.send(f"Members of Google Development Students Club sig:\n{values_str}")
 
       cursor.execute("SELECT Name FROM membersintel where grp = 'Intelligence'")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
-      await message.channel.send(f"Members of Intelligence sig  \n\n {values_str}")
+      await message.channel.send(f"Members of Intelligence sig:\n{values_str}")
 
       cursor.execute("SELECT Name FROM memberssys where grp = 'Systems'")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
-      await message.channel.send(f"Members of Systems sig  \n\n {values_str}")
+      await message.channel.send(f"Members of Systems sig:\n{values_str}")
 
       cursor.close()
       conn.close()
-
 
   if msg.startswith(".wec"):
-    if any(word in msg for word in upevents_words ):
-     
+    if any(word in msg for word in upevents_words):
       conn = mysql.connector.connect(**db_config)
       cursor = conn.cursor()
-      cursor.execute("SELECT * FROM upevents ")
+      cursor.execute("SELECT * FROM upevents")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
-      lenght = len(values_str)
+      length = len(values_str)
 
-      if(lenght==0):
-        await message.channel.send(f"No Upcomming events currently :(  \n\n ")
-
+      if length == 0:
+        await message.channel.send("No Upcoming events currently. :(")
       else:
-        await message.channel.send(f"The Upcomming events of WEC are  \n\n {values_str}")
+        await message.channel.send(f"The Upcoming events of WEC are:\n{values_str}")
 
       cursor.close()
       conn.close()
-
 
   if msg.startswith(".wec"):
     if any(word in msg for word in pevents_words):
-     
       conn = mysql.connector.connect(**db_config)
       cursor = conn.cursor()
-      cursor.execute("SELECT * FROM pevents ")
+      cursor.execute("SELECT * FROM pevents")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
-      await message.channel.send(f"The Past events of WEC are  \n\n {values_str}")
+      await message.channel.send(f"The Past events of WEC are:\n{values_str}")
 
       cursor.close()
       conn.close()
 
-
   if msg.startswith(".wec"):
-    if any(word in msg for word in con ):
-
+    if any(word in msg for word in con):
       conn = mysql.connector.connect(**db_config)
       cursor = conn.cursor()
-      cursor.execute("SELECT * FROM con ")
+      cursor.execute("SELECT * FROM con")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
       await message.channel.send(f"The Convenor of the Web club is - {values_str}")
 
       cursor.close()
-      conn.close()        
-
+      conn.close()
 
   if msg.startswith(".wec"):
     if any(word in msg for word in comeve):
-
       conn = mysql.connector.connect(**db_config)
       cursor = conn.cursor()
-
-      cursor.execute("SELECT * FROM upevents ")
+      cursor.execute("SELECT * FROM upevents")
       values = cursor.fetchall()
       values_str = '\n'.join(str(value[0]) for value in values)
-      lenght = len(values_str)
+      length = len(values_str)
 
-      if(lenght==0):
-        await message.channel.send(f"No Upcomming events currently :(  \n\n ")
-
+      if length == 0:
+        await message.channel.send("No Upcoming events currently. :(")
       else:
-        await message.channel.send(f"The Upcomming events of WEC are  \n\n {values_str}")
+        await message.channel.send(f"The Upcoming events of WEC are:\n{values_str}")
 
       cursor.close()
       conn.close()
 
-
   if msg.startswith(".wec"):
     if any(word in msg for word in thanks_words):
-      await msg.channel.send("I am Glad to hear that you liked it \n I have given the best information provided to me")
-
-
-
+      await message.channel.send("I am glad to hear that you liked it. I have provided the best information available to me.")
 
 # Runs Token
 client.run(os.getenv('TOKEN'))
+
